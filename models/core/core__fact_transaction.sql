bf903f80ad44d8798d56f55a375abd4621373858
Eric Laurello <eric.laurello@flipsidecrypto.com>
2022-07-13 16:34:42 -0400

    first set

--- models/core/core__fact_transaction.sql
+++ models/core/core__fact_transaction.sql
@@ -32,55 +32,71 @@ WHERE
         FROM
             {{ this }}
     )
-{% endif %}
-)
-SELECT
-    {{ dbt_utils.surrogate_key(
-        ['a.block_id','a.intra']
-    ) }} AS fact_transaction_id,
-    COALESCE(
-        b.dim_block_id,
-        {{ dbt_utils.surrogate_key(
-            ['null']
-        ) }}
-    ) AS dim_block_id,
-    intra,
-    tx_group_id,
-    tx_id,
-    inner_tx,
-    COALESCE(
-        da.dim_account_id,
-        {{ dbt_utils.surrogate_key(
-            ['null']
-        ) }}
-    ) AS dim_account_id__tx_sender,
-    COALESCE(
-        dim_asset_id,
-        {{ dbt_utils.surrogate_key(
-            ['null']
-        ) }}
-    ) AS dim_asset_id,
-    fee,
-    COALESCE(
-        dim_transaction_type_id,
-        {{ dbt_utils.surrogate_key(
-            ['null']
-        ) }}
-    ) AS dim_transaction_type_id,
-    tx_message,
-    extra,
-    A._inserted_timestamp
-FROM
-    base A
-    LEFT JOIN {{ ref('core__dim_block') }}
-    b
-    ON A.block_id = b.block_id
-    LEFT JOIN {{ ref('core__dim_account') }}
-    da
-    ON A.sender = da.address
-    LEFT JOIN {{ ref('core__dim_asset') }}
-    das
-    ON A.asset_id = das.asset_id
-    LEFT JOIN {{ ref('core__dim_transaction_type') }}
-    dtt
-    ON A.tx_type = dtt.tx_type
+    OR intra || block_id IN (
+        SELECT
+            intra || block_id
+        FROM
+            {{ this }}
+        WHERE
+            (
+                dim_block_id = {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+                OR dim_account_id__tx_sender = {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+                OR dim_asset_id = {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+            )
+        {% endif %}
+        SELECT
+            {{ dbt_utils.surrogate_key(
+                ['a.block_id','a.intra']
+            ) }} AS fact_transaction_id,
+            COALESCE(
+                b.dim_block_id,
+                {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+            ) AS dim_block_id,
+            intra,
+            tx_group_id,
+            tx_id,
+            inner_tx,
+            COALESCE(
+                da.dim_account_id,
+                {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+            ) AS dim_account_id__tx_sender,
+            COALESCE(
+                dim_asset_id,
+                {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+            ) AS dim_asset_id,
+            fee,
+            COALESCE(
+                dim_transaction_type_id,
+                {{ dbt_utils.surrogate_key(
+                    ['null']
+                ) }}
+            ) AS dim_transaction_type_id,
+            tx_message,
+            extra,
+            A._inserted_timestamp
+        FROM
+            base A
+            LEFT JOIN {{ ref('core__dim_block') }}
+            b
+            ON A.block_id = b.block_id
+            LEFT JOIN {{ ref('core__dim_account') }}
+            da
+            ON A.sender = da.address
+            LEFT JOIN {{ ref('core__dim_asset') }}
+            das
+            ON A.asset_id = das.asset_id
+            LEFT JOIN {{ ref('core__dim_transaction_type') }}
+            dtt
+            ON A.tx_type = dtt.tx_type
